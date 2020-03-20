9f991fb7975571f24af75e94f8b08ab8641126f3
Ahmad Faruqi <Ahmad Faruqi@10.0.0.12>
2020-03-20 17:46:26 -0400

    Venn Diagram Graphical UI Container Class

---
+++ VennDiagramTool/src/main/java/VennDiagramTool/VennGUI.java
@@ -0,0 +1,43 @@
+package VennDiagramTool;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VennGUI {
+//here we go
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VennGUI window = new VennGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VennGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
